498c4245a1b6aeda15d2021ca18e7c0efa1384a3
Markus <Markus.Schneiderm3@googlemail.com>
2023-01-04 15:06:56 +0100

    Logial Operators _ Comparisons_Lev1_15

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+let num = 5
+let anotherNum = 8
+
+console.log(num);
+console.log(anotherNum);
+
+console.log(num == anotherNum);
+console.log(num > anotherNum);
+console.log(num < anotherNum);
+console.log(num >= anotherNum);
+console.log(num <= anotherNum);
+
+console.log(num == "5");
+console.log(num != 8);
+console.log(num != "5");
